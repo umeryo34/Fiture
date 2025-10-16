28bb5efbf93f2e114f1dd7d785a4fc12619fc9d2
umeryo34 <ryo34ume@icloud.com>
2025-10-17 00:18:36 +0900

    Initial commit

---
+++ Fiture/Fiture/FitureApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitureApp.swift
+//  Fiture
+//
+//  Created by 梅澤遼 on 2025/10/16.
+//
+
+import SwiftUI
+
+@main
+struct FitureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
